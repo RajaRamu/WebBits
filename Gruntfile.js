9c72f13f5bb486da3368e2f24e393946e61a9e78
lmarkus <lmarkus@paypal.com>
2015-10-02 10:51:31 -0700

    Added archive indexing to workflow

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by lmarkus on 10/2/15.
+ */
+'use strict';
+var fs = require('fs'),
+    path = require('path');
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        index: {
+            main: {
+                archiveDirectory: 'archive/'
+            }
+        }
+    })
+
+    grunt.registerMultiTask('index', 'Create the index of the archive as a JSON file', generateIndex)
+}
+
+function generateIndex() {
+    var archives = fs
+        .readdirSync(this.data.archiveDirectory)
+        .filter(function (file) {
+            return (path.extname(file.toLowerCase()) === '.json');
+        });
+    fs.writeFileSync('archives.json', JSON.stringify(archives));
+}
