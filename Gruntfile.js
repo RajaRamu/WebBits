dadc124c6f92f0710879497c72f57283e54818c1
lmarkus <lmarkus@paypal.com>
2015-10-02 11:04:45 -0700

    Strip JSON extension in archive index

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,17 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('index', 'Create the index of the archive as a JSON file', generateIndex)
 }
 
+/**
+ * Generate a list of the files in the archives (Filtering and Stripping the JSON extension)
+ */
 function generateIndex() {
     var archives = fs
         .readdirSync(this.data.archiveDirectory)
         .filter(function (file) {
             return (path.extname(file.toLowerCase()) === '.json');
+        })
+        .map(function(file){
+            return file.split('\.json')[0];
         });
     fs.writeFileSync('archives.json', JSON.stringify(archives));
 }
